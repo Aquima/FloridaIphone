7c3267c619f56b08feb08d2e7ed59f5681e7eb01
Raul Quispe <Quima@Aquima.local>
2014-09-08 16:57:48 -0500

    Initial Commit

---
+++ Florida/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Florida
+//
+//  Created by Raul Quispe on 9/8/14.
+//  Copyright (c) 2014 kodebinario. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Florida/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Florida
+//
+//  Created by Raul Quispe on 9/8/14.
+//  Copyright (c) 2014 kodebinario. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
